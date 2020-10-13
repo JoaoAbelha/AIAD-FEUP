dd72f87ac381b32e2c7245dfdf20e75bde739b24
JoaoAbelha <jpabelha@hotmail.com>
2020-10-13 22:04:35 +0100

    Hello Agent working

---
+++ src/com/Agent/HelloAgent.java
@@ -0,0 +1,11 @@
+package com.Agent;
+
+import jade.core.Agent;
+
+public class HelloAgent extends Agent {
+
+    @Override
+    protected void setup() {
+        System.out.println("First agent");
+    }
+}
